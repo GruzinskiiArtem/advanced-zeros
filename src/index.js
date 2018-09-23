c8dff7b1d0c83f242ee6be935a66b551c9f3140f
GruzinskiiArtem <m1nck@yandex.ru>
2018-09-23 23:06:45 +0300

    The calculation of zeros at the end of the number is added.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    var bases = [];
+    var i = 2;
+    var j = 1;
+    var result = [];
+
+    while (i <= base) {
+        if (base % i === 0) {
+            bases.push(i);
+            base = base / i;
+        }
+        else {
+            i++
+        }
+    }
+
+    bases.reverse();
+    bases.forEach(function (value, index) {
+        var sum = 0;
+        var n = number;
+        while (n >= 1) {
+            var temp = parseInt(n / value);
+            n = temp;
+            sum = sum + temp;
+
+        }
+        if (value === bases[index + 1])
+        {
+            j++;
+        }
+        result.push(Math.floor(sum / j));
+    });
+    return Math.min.apply(null, result);
 }
